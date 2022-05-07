e8656f094cb0f5cd29c46919c38a7723c4e320c0
lee unseo <lee.unseo17@gmail.com>
2022-05-07 23:44:21 +0900

    Minimal request

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+use reqwest::Result;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let base_url = "https://keirin.kdreams.jp/kaisai/2022/05/01/";
+    let body = reqwest::get(base_url).await?.text().await?;
+    println!("{:?}", body);
+    Ok(())
+}
